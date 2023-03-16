76fc7130a96515d0d7403bb21565341607c6fa79
Pavel Rovnyi <rovniyp@gmail.com>
2023-03-16 23:24:08 +0200

    investigate VUE

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Form from './components/Form.vue';
+const rootComponentValue = ref(1);
+
+const addValue = () => {
+  rootComponentValue.value++;
+  console.log(rootComponentValue.value);
+};
+
+const updateRootValue = (newVal: number) => {
+  rootComponentValue.value = newVal;
+};
 </script>
 
 <template>
-  <header> 
-
-    <div class="wrapper">
-      <HelloWorld msg="My first VUE APP" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container mx-auto py-2">
+    <Form :value="rootComponentValue" @update-internal-value="updateRootValue" />
+    <button @click="addValue">Add</button>
+  </div>
 </template>
-
