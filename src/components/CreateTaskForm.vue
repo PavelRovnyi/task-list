535f3b591c844203a366b90be0b4df8e8baa39ac
Pavel Rovnyi <rovniyp@gmail.com>
2023-03-22 15:08:33 +0200

    WIP set components by functionality

--- src/components/CreateTaskForm.vue
+++ src/components/CreateTaskForm.vue
@@ -1,33 +1,32 @@
 <template>
   <div class="flex items-center justify-between mb-2">
     <input
+      v-model="newTask"
       type="text"
-      name="search"
-      id="search"
+      name="create-task"
+      id="create-task"
       autocomplete="off"
-      placeholder="Enter task name to search"
+      placeholder="Enter task name"
       class="mr-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-teal-500 block w-full rounded-md sm:text-sm invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
     /><button
-      title="Add"
-      class=""
+      title="Add Task"
+      class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
-      <svg
-        class="svg-inline--fa fa-circle-plus fa-xl"
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fas"
-        data-icon="circle-plus"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-      >
-        <path
-          class=""
-          fill="currentColor"
-          d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
-        ></path>
-      </svg>
+      Create
     </button>
   </div>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { ref } from 'vue' 
+
+const tasks = ref<string[]>([])
+const newTask = ref<string>('')
+
+const addTask = () => {
+  if (newTask.value) {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+</script>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -6,10 +6,8 @@ import Card from './Card.vue'
     <div class="mx-auto">
       <ul
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-4 text-gray-500 list-inside dark:text-gray-400"
-      >
-        <li class="bg-white min-h-[200px] p-3 border rounded-lg shadow">
-          <Card></Card>
-        </li>
+      ><Card></Card>
+       
       </ul>
     </div>
   </div>
